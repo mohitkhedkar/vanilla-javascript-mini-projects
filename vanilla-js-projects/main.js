a562472e21d0af505fd22422dcbe87f57e6812c3
Mohit Khedkar <mohitkhedkar521@gmail.com>
2023-10-09 03:19:18 +0530

    Site Update

---
+++ vanilla-js-projects/main.js
@@ -0,0 +1,6 @@
+import './styles/style.css';
+import './styles/components/header.css';
+import './styles/components/mainsection.css';
+import './styles/components/contact.css';
+import './styles/components/footer.css';
+import './styles/utis.css';
