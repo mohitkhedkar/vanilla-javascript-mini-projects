f8a135564ef781ac4904be68aacda8b9937cf22a
Mohit Khedkar <mohitkhedkar521@gmail.com>
2024-01-30 03:08:40 +0530

    color switcher project added

---
+++ projects/color changer/script.js
@@ -0,0 +1,19 @@
+const btns = document.querySelectorAll(".btn");
+const body = document.querySelector("body");
+
+btns.forEach(function (btns) {
+  btns.addEventListener("click", (e) => {
+    if (e.target.id === "red") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "green") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
